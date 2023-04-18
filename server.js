9db9f7f20c24f269207e989b8652870c6140c38b
bpolishevsk <bpolishevsk@gmail.com>
2023-04-18 22:29:06 +0800

    add some features

--- server.js
+++ server.js
@@ -6,13 +6,10 @@ const path = require("path");
 require("dotenv").config();
 //middlewares
 
-const port =
-  process.env.NODE_ENV === "production"
-    ? process.env.PRODUCTION_API_PORT
-    : process.env.DEV_API_PORT;
+const port = process.env.PRODUCTION_API_PORT;
 
 mongoose.on("connected", () => {
-  app.listen(port, () => {
+  app.listen(port || 8000, () => {
     require("./routes/routes")(app);
     console.log("----------Metric LogoBook -----------");
     console.log(`Server app listening on port ${port}`);
